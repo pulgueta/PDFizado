a1fa29d1d87458e8fd5eab18aa52161820abd011
pulgueta <retardix456@gmail.com>
2023-11-16 02:17:11 -0500

    Update PDFLoader component with new Skeleton import and responsive width

--- components/server/pdf-loaders.tsx
+++ components/server/pdf-loaders.tsx
@@ -1,8 +1,8 @@
-import { Skeleton } from '../ui/skeleton';
+import { Skeleton } from '~/shadcn/skeleton';
 
 export const PDFLoader = () => {
     return Array.from({ length: 3 }, (_, i) => (
-        <div key={i} className='mx-auto w-96 rounded-2xl border p-4'>
+        <div key={i} className='mx-auto w-80 rounded-2xl border p-4 md:w-96'>
             <Skeleton className='mb-4 h-10' />
 
             <Skeleton className='mb-2 h-6' />
