265271323b50a512aff09ed6af9639fcebe374ff
pulgueta <retardix456@gmail.com>
2023-11-16 15:37:53 -0500

    Remove unused user API route

--- app/api/user/route.ts
+++
@@ -1,7 +0,0 @@
-import { type NextRequest, NextResponse } from 'next/server';
-
-export const GET = async (req: NextRequest) => {
-    const body = await req.json();
-
-    return NextResponse.json({ body }, { status: 200 });
-};
