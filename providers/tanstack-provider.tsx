62dd7371b6fe29e0d76251ddb633dfb13e18c145
pulgueta <retardix456@gmail.com>
2023-11-15 21:26:32 -0500

    Refactor ReactQueryProvider in tanstack-provider.tsx.

--- providers/tanstack-provider.tsx
+++ providers/tanstack-provider.tsx
@@ -1,29 +1,15 @@
 'use client';
 
-import { useState } from 'react';
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
 
 import { Layout } from '~/types';
 
+const queryClient = new QueryClient();
+
 const ReactQueryProvider: React.FC<Layout> = ({ children }) => {
-    const [queryClient] = useState(
-        () =>
-            new QueryClient({
-                defaultOptions: {
-                    queries: {
-                        refetchOnWindowFocus: false,
-                        retry: false,
-                    },
-                },
-            })
-    );
     return (
         <QueryClientProvider client={queryClient}>
-            <ReactQueryStreamedHydration>
-                {children}
-            </ReactQueryStreamedHydration>
+            {children}
         </QueryClientProvider>
     );
 };
