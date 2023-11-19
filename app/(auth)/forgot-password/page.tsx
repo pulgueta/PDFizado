846eb1d53065575cc9afdfb1f0707fd6d2a28936
pulgueta <retardix456@gmail.com>
2023-11-19 04:56:13 -0500

    Refactor forgot password page layout and add email input validation

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -10,9 +10,15 @@ import { Input } from '~/shadcn/input';
 import { Label } from '~/shadcn/label';
 
 const ForgotPassword = () => {
+    const action = async (e: FormData) => {
+        'use server';
+        const email = e.get('email');
+
+        console.log(email);
+    };
     return (
         <section className='flex min-h-[calc(100vh-80px)] items-center justify-center'>
-            <Card className='max-w-sm'>
+            <Card className='max-w-md'>
                 <CardHeader>
                     <CardTitle>Recuperar contraseña</CardTitle>
                     <CardDescription>
@@ -21,18 +27,19 @@ const ForgotPassword = () => {
                     </CardDescription>
                 </CardHeader>
                 <CardContent>
-                    <form className='space-y-6'>
-                        <div>
+                    <form
+                        action={action}
+                        className='flex flex-col items-center gap-y-4'
+                    >
+                        <div className='w-full space-y-2'>
                             <Label htmlFor='email'>Correo electrónico</Label>
-                            <div className='mt-2'>
-                                <Input
-                                    id='email'
-                                    name='email'
-                                    type='email'
-                                    autoComplete='email'
-                                    required
-                                />
-                            </div>
+                            <Input
+                                id='email'
+                                name='email'
+                                type='email'
+                                autoComplete='email'
+                                required
+                            />
                         </div>
 
                         <Button className='w-full'>Recuperar contraseña</Button>
