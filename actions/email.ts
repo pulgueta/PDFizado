c2f8a22239e84f52e3abfa2b7065c0c82b4c5fa2
pulgueta <retardix456@gmail.com>
2023-11-19 05:38:53 -0500

    Update email API URL

--- actions/email.ts
+++ actions/email.ts
@@ -3,7 +3,7 @@
 export const sendEmail = async (e: FormData) => {
     const email = e.get('email') as string;
 
-    await fetch(`${process.env.VERCEL_URL}/api/email`, {
+    await fetch(`${process.env.BASE_URL}/api/email`, {
         method: 'POST',
         body: JSON.stringify({ email }),
     });
