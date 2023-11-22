2325eeacfc175bbb2351c18040208afa300463f0
pulgueta <retardix456@gmail.com>
2023-11-21 20:15:36 -0500

    Fix token retrieval and add conditional URL for production

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -13,12 +13,15 @@ const ResetPassword = async ({ searchParams }: ResetToken) => {
 		},
 	});
 
+	const token = searchParams.token;
+
 	const isTokenExpired =
 		dbToken?.expires.toISOString()! < new Date().toISOString();
 
 	return (
 		<section className='flex min-h-[calc(100vh-205px)] flex-col items-center justify-center gap-y-4'>
 			<pre>{JSON.stringify(dbToken, null, 2)}</pre>
+			<h1>Token: {token}</h1>
 			<h1>Token expiration date: {dbToken?.expires.toDateString()}</h1>
 			<h1>Token is expired: {isTokenExpired ? 'Yes' : 'No'}</h1>
 		</section>
