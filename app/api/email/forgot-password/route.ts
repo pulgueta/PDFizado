e66f7f321b678b48164cd464564234fd3c45d20c
pulgueta <retardix456@gmail.com>
2023-11-21 20:44:11 -0500

    Fix forgot password token URL

--- app/api/email/forgot-password/route.ts
+++ app/api/email/forgot-password/route.ts
@@ -37,9 +37,7 @@ export const POST = async (req: NextRequest) => {
 	}
 
 	const { token } = await fetch(
-		process.env.NODE_ENV === 'development'
-			? `http://localhost:3000/api/email/forgot-password/token/${isUserCreated.id}`
-			: `${process.env.VERCEL_URL}/api/email/forgot-password/token/${isUserCreated.id}`,
+		`http://localhost:3000/api/email/forgot-password/token/${isUserCreated.id}`,
 		{
 			method: 'POST',
 			body: JSON.stringify({ id: isUserCreated.id }),
