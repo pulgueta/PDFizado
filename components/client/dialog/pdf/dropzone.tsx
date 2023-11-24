0e795e78c4ef2dfe4f3a3e35e88b3abc189d5082
pulgueta <retardix456@gmail.com>
2023-11-24 06:14:28 -0500

    Refactor PDFCard component

--- components/client/dialog/pdf/dropzone.tsx
+++ components/client/dialog/pdf/dropzone.tsx
@@ -1,11 +1,13 @@
-import { useParams, useRouter } from 'next/navigation';
+'use client';
+
+import { usePathname, useRouter } from 'next/navigation';
 
 import { useMutation } from '@tanstack/react-query';
 import {
-    CheckCircle2Icon,
-    FileIcon,
-    Loader2Icon,
-    UploadCloudIcon,
+	CheckCircle2Icon,
+	FileIcon,
+	Loader2Icon,
+	UploadCloudIcon,
 } from 'lucide-react';
 import { useSession } from 'next-auth/react';
 import { useDropzone } from 'react-dropzone';
@@ -15,143 +17,141 @@ import { uploadToS3 } from '~/lib/aws/awsS3';
 import { maxSizeAllowed } from '~/lib/plan-allowance';
 
 interface Mutation {
-    key: string;
-    name: string;
-    url: string;
+	key: string;
+	name: string;
+	url: string;
 }
 
 export const Dropzone = () => {
-    const { push } = useRouter();
-    const { id: userId } = useParams();
+	const { push } = useRouter();
+	const pathname = usePathname();
 
-    const { mutate, isPending, isSuccess } = useMutation({
-        mutationKey: ['uploadToS3'],
-        mutationFn: async ({ key, name, url }: Mutation) => {
-            const data = await fetch('/api/files', {
-                body: JSON.stringify({
-                    key,
-                    name,
-                    url,
-                }),
-                method: 'POST',
-            });
+	const { mutate, isPending, isSuccess } = useMutation({
+		mutationKey: ['uploadToS3'],
+		mutationFn: ({ key, name, url }: Mutation) => {
+			const data = fetch('/api/files', {
+				body: JSON.stringify({
+					key,
+					name,
+					url,
+				}),
+				method: 'POST',
+			}).then((res) => res.json());
 
-            const response = await data.json();
+			return data;
+		},
+		onError: (err) => {
+			toast.error(err.message || 'Error al subir el PDF');
+		},
+		onSuccess: ({ id }) => {
+			toast.success(
+				'Tu PDF se ha procesado correctamente, serás redirigido en unos segundos'
+			);
+			push(`${pathname}/${id}`);
+		},
+		retry: 3,
+		retryDelay: 1000,
+	});
 
-            return response;
-        },
-        onError: (err) => {
-            toast.error(err.message || 'Error al subir el PDF');
-        },
-        onSuccess: ({ id }) => {
-            toast.success(
-                'Tu PDF se ha procesado correctamente, serás redirigido en unos segundos'
-            );
-            push(`/dashboard/${userId}/${id}`);
-        },
-        retry: 3,
-        retryDelay: 1000,
-    });
+	const session = useSession();
 
-    const session = useSession();
+	const plan = session.data?.user.plan as string;
 
-    const plan = session.data?.user.plan as string;
+	const { getRootProps, getInputProps, acceptedFiles } = useDropzone({
+		accept: { 'application/pdf': ['.pdf'] },
+		maxFiles: 1,
+		multiple: false,
+		maxSize: maxSizeAllowed(plan),
+		validator: (file) => {
+			if (file.size > maxSizeAllowed(plan)) {
+				return {
+					code: 'file-too-large',
+					message: 'El archivo excede el límite de tu plan',
+				};
+			}
 
-    const { getRootProps, getInputProps, acceptedFiles } = useDropzone({
-        accept: { 'application/pdf': ['.pdf'] },
-        maxFiles: 1,
-        multiple: false,
-        maxSize: maxSizeAllowed(plan),
-        validator: (file) => {
-            if (file.size > maxSizeAllowed(plan)) {
-                return {
-                    code: 'file-too-large',
-                    message: 'El archivo excede el límite de tu plan',
-                };
-            }
+			if (!file.type.includes('pdf')) {
+				return {
+					code: 'file-invalid-type',
+					message: 'El archivo no es un PDF',
+				};
+			}
 
-            if (!file.type.includes('pdf')) {
-                return {
-                    code: 'file-invalid-type',
-                    message: 'El archivo no es un PDF',
-                };
-            }
+			return null;
+		},
+		onDropRejected: (fileRejections) => {
+			toast.error(fileRejections[0].errors[1].message);
+		},
+		onError: (err) => {
+			toast.error(err.message);
+		},
+		onDropAccepted: (file) => {
+			toast.promise(
+				async () => {
+					const res = await uploadToS3(file[0]);
 
-            return null;
-        },
-        onDropRejected: (fileRejections) => {
-            toast.error(fileRejections[0].errors[1].message);
-        },
-        onError: (err) => {
-            toast.error(err.message);
-        },
-        onDropAccepted: (file) => {
-            toast.promise(
-                async () => {
-                    const res = await uploadToS3(file[0]);
+					if (!res.key || !res.name) {
+						toast.error('Error al subir el PDF');
+					}
 
-                    if (!res.key || !res.name) {
-                        toast.error('Error al subir el PDF');
-                    }
+					mutate(res);
+				},
+				{
+					loading: `Subiendo ${file[0].name}...`,
+					success: `${file[0].name} subido correctamente`,
+					error: `Error al subir ${file[0].name}`,
+				}
+			);
+		},
+	});
 
-                    mutate(res);
-                },
-                {
-                    loading: `Subiendo ${file[0].name}...`,
-                    success: `${file[0].name} subido correctamente`,
-                    error: `Error al subir ${file[0].name}`,
-                }
-            );
-        },
-    });
+	return (
+		<div
+			{...getRootProps({
+				className:
+					'mx-auto my-4 w-64 cursor-pointer rounded border border-dashed border-gray-300 bg-neutral-100 px-4 py-8 transition-colors duration-200 ease-in-out hover:bg-neutral-200 dark:bg-neutral-900 dark:hover:bg-neutral-800 md:w-full md:py-16',
+			})}
+		>
+			<input {...getInputProps()} disabled={isPending || isSuccess} />
+			<>
+				{isSuccess ? (
+					<>
+						<CheckCircle2Icon className='mx-auto h-8 w-8 text-green-400' />
+						<p className='mt-2 text-center font-medium'>
+							Se ha procesado tu PDF, redirigiendo...
+						</p>
+					</>
+				) : !isPending ? (
+					<>
+						<UploadCloudIcon className='mx-auto h-8 w-8' />
+						<p className='my-2 text-center font-medium'>
+							Arrastra aquí tu PDF
+						</p>
+					</>
+				) : (
+					<>
+						<Loader2Icon className='mx-auto h-8 w-8 animate-spin' />
+						<p className='my-2 text-center font-medium'>
+							Procesando PDF...
+						</p>
+						{acceptedFiles && acceptedFiles[0] && (
+							<div className='mx-auto flex max-w-sm items-center gap-4 overflow-hidden rounded border p-4'>
+								<FileIcon className='h-6 w-6 text-primary' />
 
-    return (
-        <div
-            {...getRootProps({
-                className:
-                    'mx-auto my-4 w-64 cursor-pointer rounded border border-dashed border-gray-300 bg-neutral-100 px-4 py-8 transition-colors duration-200 ease-in-out hover:bg-neutral-200 dark:bg-neutral-900 dark:hover:bg-neutral-800 md:w-full md:py-16',
-            })}
-        >
-            <input {...getInputProps()} disabled={isPending || isSuccess} />
-            <>
-                {isSuccess ? (
-                    <>
-                        <CheckCircle2Icon className='mx-auto h-8 w-8 text-green-400' />
-                        <p className='mt-2 text-center font-medium'>
-                            Se ha procesado tu PDF, redirigiendo...
-                        </p>
-                    </>
-                ) : !isPending ? (
-                    <>
-                        <UploadCloudIcon className='mx-auto h-8 w-8' />
-                        <p className='my-2 text-center font-medium'>
-                            Arrastra aquí tu PDF
-                        </p>
-                    </>
-                ) : (
-                    <>
-                        <Loader2Icon className='mx-auto h-8 w-8 animate-spin' />
-                        <p className='my-2 text-center font-medium'>
-                            Procesando PDF...
-                        </p>
-                        {acceptedFiles && acceptedFiles[0] && (
-                            <div className='mx-auto flex max-w-sm items-center gap-4 overflow-hidden rounded border p-4'>
-                                <FileIcon className='h-6 w-6 text-primary' />
-
-                                <span className='truncate text-center text-sm text-muted-foreground'>
-                                    {acceptedFiles[0].name}
-                                </span>
-                                <span className='text-center text-sm text-muted-foreground'>
-                                    {Number(
-                                        acceptedFiles[0].size / 1000000
-                                    ).toFixed(2)}
-                                    MB{' '}
-                                </span>
-                            </div>
-                        )}
-                    </>
-                )}
-            </>
-        </div>
-    );
+								<span className='truncate text-center text-sm text-muted-foreground'>
+									{acceptedFiles[0].name}
+								</span>
+								<span className='text-center text-sm text-muted-foreground'>
+									{Number(
+										acceptedFiles[0].size / 1000000
+									).toFixed(2)}
+									MB{' '}
+								</span>
+							</div>
+						)}
+					</>
+				)}
+			</>
+		</div>
+	);
 };
--- components/client/pdf-card.tsx
+++ components/client/pdf-card.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { useParams } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 
 import { File } from '@prisma/client';
 import { toast } from 'sonner';
@@ -7,66 +7,71 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { Loader2Icon } from 'lucide-react';
 
 import { Button, buttonVariants } from '~/shadcn/button';
+import { cn } from '~/lib/utils';
 
 export const PDFCard: React.FC<File> = (file) => {
-    const { id } = useParams();
+	const pathname = usePathname();
 
-    const queryClient = useQueryClient();
-    const { mutate, isPending } = useMutation({
-        mutationKey: ['deleteFile'],
-        mutationFn: async (id: string) => {
-            const res = await fetch('/api/files', {
-                method: 'DELETE',
-                body: JSON.stringify({ id, key: file.awsKey }),
-            });
+	const queryClient = useQueryClient();
+	const { mutate, isPending } = useMutation({
+		mutationKey: ['deleteFile'],
+		mutationFn: (id: string) => {
+			const files = fetch('/api/files', {
+				method: 'DELETE',
+				body: JSON.stringify({ id, key: file.awsKey }),
+			}).then((res) => res.json());
 
-            const files = await res.json();
+			return files;
+		},
+		onSuccess: () => {
+			queryClient.invalidateQueries({
+				queryKey: ['files'],
+			});
+			toast.success('PDF eliminado correctamente');
+		},
+		onError: () => toast.error('Error al eliminar el PDF'),
+		retry: 3,
+		retryDelay: 1000,
+	});
 
-            return files;
-        },
+	const onDeleteFile = (id: string) => () => mutate(id);
 
-        onSuccess: () => {
-            queryClient.invalidateQueries({
-                queryKey: ['files'],
-            });
-            toast.success('PDF eliminado correctamente');
-        },
-        onError: () => {
-            toast.error('Error al eliminar el PDF');
-        },
-    });
+	return (
+		<div className='mx-auto flex w-80 flex-col rounded-2xl border p-4 md:w-96'>
+			<h3 className='truncate text-xl font-semibold'>{file.name}</h3>
+			<span className='my-4 text-muted-foreground'>
+				Fecha de subida:{' '}
+				{new Date(file.createdAt).toLocaleDateString('es-ES', {
+					year: 'numeric',
+					month: 'long',
+					day: 'numeric',
+				})}
+			</span>
 
-    const onDeleteFile = (id: string) => () => mutate(id);
-
-    return (
-        <div className='mx-auto flex w-80 flex-col rounded-2xl border p-4 md:w-96'>
-            <h3 className='truncate text-xl font-semibold'>{file.name}</h3>
-            <span className='my-4 text-muted-foreground'>
-                Fecha de subida:{' '}
-                {new Date(file.createdAt).toLocaleDateString('es-ES', {
-                    year: 'numeric',
-                    month: 'long',
-                    day: 'numeric',
-                })}
-            </span>
-
-            <div className='flex w-full items-center justify-between border-t pt-4'>
-                <Link
-                    href={`/dashboard/${id}/${file.id}`}
-                    className={buttonVariants({
-                        variant: 'ghost',
-                    })}
-                >
-                    Ver PDF
-                </Link>
-                <Button variant='destructive' onClick={onDeleteFile(file.id)}>
-                    {isPending ? (
-                        <Loader2Icon className='h-4 w-4 animate-spin' />
-                    ) : (
-                        'Eliminar PDF'
-                    )}
-                </Button>
-            </div>
-        </div>
-    );
+			<div className='flex w-full items-center justify-between border-t pt-4'>
+				<Link
+					href={`${pathname}/${file.id}`}
+					className={cn(
+						buttonVariants({
+							variant: 'ghost',
+						}),
+						isPending && 'cursor-not-allowed'
+					)}
+				>
+					Ver PDF
+				</Link>
+				<Button
+					variant='destructive'
+					disabled={isPending}
+					onClick={onDeleteFile(file.id)}
+				>
+					{isPending ? (
+						<Loader2Icon className='h-4 w-4 animate-spin' />
+					) : (
+						'Eliminar PDF'
+					)}
+				</Button>
+			</div>
+		</div>
+	);
 };
