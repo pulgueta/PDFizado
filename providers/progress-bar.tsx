d34107cd7644ce4b1cd739fd4137e39bd5fc9b4c
pulgueta <retardix456@gmail.com>
2023-12-02 17:51:32 -0500

    Add Suspense to ProgressProvider component

--- providers/progress-bar.tsx
+++ providers/progress-bar.tsx
@@ -1,18 +1,22 @@
 'use client';
 
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, Suspense } from 'react';
 
 import { AppProgressBar } from 'next-nprogress-bar';
+
+const Fallback = () => <></>;
 
 export const ProgressProvider = ({ children }: PropsWithChildren) => {
 	return (
 		<>
 			{children}
-			<AppProgressBar
-				options={{ showSpinner: false }}
-				color='#e11d48'
-				shallowRouting
-			/>
+			<Suspense fallback={<Fallback />}>
+				<AppProgressBar
+					options={{ showSpinner: false }}
+					color='#e11d48'
+					shallowRouting
+				/>
+			</Suspense>
 		</>
 	);
 };
