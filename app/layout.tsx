3f7399c3efcaacc5074fa898b1e2cdce2add9bc5
pulgueta <retardix456@gmail.com>
2023-12-02 17:30:23 -0500

    Add ProgressProvider to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import { Toaster } from 'sonner';
 import { ThemeProvider } from '~/providers/theme-provider';
 import { AuthProvider } from '~/providers/auth-session';
 import { ReactQueryProvider } from '~/providers/tanstack-provider';
+import { ProgressProvider } from '~/providers/progress-bar';
 import { Footer } from '~/components/server/footer/footer';
 import { Navbar } from '~/components/client/navbar/navbar';
 
@@ -122,10 +123,16 @@ const RootLayout = ({ children }: PropsWithChildren) => (
 			<ThemeProvider attribute='class' defaultTheme='system' enableSystem>
 				<AuthProvider>
 					<ReactQueryProvider>
-						<Navbar />
-						<Toaster richColors closeButton position='top-center' />
-						{children}
-						<Footer />
+						<ProgressProvider>
+							<Navbar />
+							<Toaster
+								richColors
+								closeButton
+								position='top-center'
+							/>
+							{children}
+							<Footer />
+						</ProgressProvider>
 					</ReactQueryProvider>
 				</AuthProvider>
 			</ThemeProvider>
---
+++ providers/progress-bar.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { PropsWithChildren } from 'react';
+
+import { AppProgressBar } from 'next-nprogress-bar';
+
+export const ProgressProvider = ({ children }: PropsWithChildren) => {
+	return (
+		<>
+			{children}
+			<AppProgressBar options={{ showSpinner: false }} color='#e11d48' />
+		</>
+	);
+};
