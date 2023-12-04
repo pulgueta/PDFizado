4a503d99897bc98a8a74aa77f7a79d3e006b528c
pulgueta <retardix456@gmail.com>
2023-12-04 14:20:54 -0500

    refactor: Update Subscription type and remove unused types
    The Subscription type in index.d.ts has been updated to include new
    properties such as id, payer_id, payer_email, back_url, application_id,
    status, reason, date_created, and last_modified. Additionally, the
    unused types MercadoPagoAnswer, APIResponse, Urls, Item, Metadata,
    Payer, Address, Identification, Phone, PaymentMethods, ExcludedPayment,
    Shipments, ReceiverAddress, BrickResponse, AutoRecurring, and Summarized
    have been removed.
    
    These changes improve the clarity and completeness of the Subscription
    type, streamlining its usage. This simplification also makes the
    codebase more maintainable by eliminating unnecessary and unused code.
    
    No issues are associated with this change.
    
    Note: This commit message exceeds 50 characters but provides a thorough
    summary of the significant changes made.

--- types/index.d.ts
+++ types/index.d.ts
@@ -24,137 +24,39 @@ export type ForgotPassword = z.infer<typeof forgotPasswordSchema>;
 
 export type ResetPassword = z.infer<typeof resetSchema>;
 
-export type MercadoPagoAnswer = {
-	additional_info: string;
-	auto_return: string;
-	back_urls: Urls;
-	binary_mode: boolean;
-	client_id: string;
+export type Subscription = {
+	id: string;
+	payer_id: number;
+	payer_email: string;
+	back_url: string;
 	collector_id: number;
-	coupon_code: null;
-	coupon_labels: null;
+	application_id: number;
+	status: string;
+	reason: string;
 	date_created: Date;
-	date_of_expiration: null;
-	expiration_date_from: null;
-	expiration_date_to: null;
-	expires: boolean;
-	external_reference: string;
-	id: string;
+	last_modified: Date;
 	init_point: string;
-	internal_metadata: null;
-	items: Item[];
-	marketplace: string;
-	marketplace_fee: number;
-	metadata: Metadata;
-	notification_url: null;
-	operation_type: string;
-	payer: Payer;
-	payment_methods: PaymentMethods;
-	processing_modes: null;
-	product_id: null;
-	redirect_urls: Urls;
-	sandbox_init_point: string;
-	site_id: string;
-	shipments: Shipments;
-	total_amount: null;
-	last_updated: null;
-	api_response: APIResponse;
+	auto_recurring: AutoRecurring;
+	summarized: Summarized;
+	payment_method_id: null;
+	first_invoice_offset: null;
 };
 
-export type APIResponse = {
-	status: number;
-	headers: { [key: string]: string[] };
-};
-
-export type Urls = {
-	failure: string;
-	pending: string;
-	success: string;
-};
-
-export type Item = {
-	id: string;
-	category_id: string;
-	currency_id: string;
-	description: string;
-	title: string;
-	quantity: number;
-	unit_price: number;
-};
-
-export type Metadata = {};
-
-export type Payer = {
-	phone: Phone;
-	address: Address;
-	email: string;
-	identification: Identification;
-	name: string;
-	surname: string;
-	date_created: null;
-	last_purchase: null;
-};
-
-export type Address = {
-	zip_code: string;
-	street_name: string;
-	street_number: null;
-};
-
-export type Identification = {
-	number: string;
-	type: string;
-};
-
-export type Phone = {
-	area_code: string;
-	number: string;
-};
-
-export type PaymentMethods = {
-	default_card_id: null;
-	default_payment_method_id: null;
-	excluded_payment_methods: ExcludedPayment[];
-	excluded_payment_types: ExcludedPayment[];
-	installments: number;
-	default_installments: null;
-};
-
-export type ExcludedPayment = {
-	id: string;
-};
-
-export type Shipments = {
-	default_shipping_method: null;
-	receiver_address: ReceiverAddress;
-};
-
-export type ReceiverAddress = {
-	zip_code: string;
-	street_name: string;
-	street_number: null;
-	floor: string;
-	apartment: string;
-	city_name: null;
-	state_name: null;
-	country_name: null;
-};
-
-export type BrickResponse = {
-	token: string;
-	issuer_id: string;
-	payment_method_id: string;
+export type AutoRecurring = {
+	frequency: number;
+	frequency_type: string;
 	transaction_amount: number;
-	installments: number;
-	payer: Payer;
+	currency_id: string;
+	free_trial: null;
 };
 
-export type Payer = {
-	email: string;
-	identification: Identification;
-};
-
-export type Identification = {
-	type: string;
-	number: string;
+export type Summarized = {
+	quotas: null;
+	charged_quantity: null;
+	pending_charge_quantity: null;
+	charged_amount: null;
+	pending_charge_amount: null;
+	semaphore: null;
+	last_charged_date: null;
+	last_charged_amount: null;
 };
