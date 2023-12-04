a9fb6d0b2a13209db7b61577a2b1812c69fdcf5f
pulgueta <retardix456@gmail.com>
2023-12-04 14:32:17 -0500

    Refactor MercadoPago webhook route to handle subscription status
    This commit refactors the MercadoPago webhook route to handle
    subscription status updates. It now retrieves the preapproval ID from
    the request body and makes a request to the MercadoPago API to fetch the
    current status. If the status is 'authorized', it logs a message
    indicating an upgrade to the pro subscription. If the status is
    'cancelled', it logs a message indicating a downgrade to the free tier.
    This change improves the handling of subscription events in the
    application, ensuring accurate tracking and appropriate actions based on
    the status.
    
    No issue references.

--- app/api/webhooks/mercadopago/route.ts
+++ app/api/webhooks/mercadopago/route.ts
@@ -1,9 +1,32 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+import { env } from '~/env';
+
 export const POST = async (req: NextRequest) => {
 	const body = await req.json();
 
-	console.log(body);
+	console.log('webhook', body);
+
+	if (body.data.id) {
+		const res = await fetch(
+			`https://api.mercadopago.com/preapproval/${body.data.id}`,
+			{
+				headers: {
+					Authorization: `Bearer ${env.MERCADOPAGO_SECRET}`,
+				},
+			}
+		).then((res) => res.json());
+
+		console.log('subscription status', res);
+
+		if (res.status === 'authorized') {
+			console.log('upgrading subscription to pro');
+		}
+
+		if (res.status === 'cancelled') {
+			console.log('downgrading to free tier');
+		}
+	}
 
 	return NextResponse.json(body, { status: 200 });
 };
