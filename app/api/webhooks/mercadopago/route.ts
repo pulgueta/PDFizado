9ce1fc1a1adb0931aa1cb78216a561b9e213a99b
pulgueta <retardix456@gmail.com>
2023-12-04 18:04:15 -0500

    refactor: Added the proper response types for the requests and modified the switch case for better readability

--- app/api/webhooks/mercadopago/route.ts
+++ app/api/webhooks/mercadopago/route.ts
@@ -1,38 +1,39 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 import { env } from '~/env';
+import { Subscription, Webhook } from './types';
 
 export const POST = async (req: NextRequest) => {
-	const body = await req.json();
+	const body = (await req.json()) as Webhook;
 
-	console.log('webhook', body);
+	try {
+		if (body.data.id) {
+			const res = (await fetch(
+				`https://api.mercadopago.com/preapproval/${body.data.id}`,
+				{
+					headers: {
+						Authorization: `Bearer ${env.MERCADOPAGO_SECRET}`,
+					},
+				}
+			)
+				.then((res) => res.json())
+				.catch((err) => console.log(err))) as Subscription;
 
-	if (body.data.id) {
-		const res = await fetch(
-			`https://api.mercadopago.com/preapproval/${body.data.id}`,
-			{
-				headers: {
-					Authorization: `Bearer ${env.MERCADOPAGO_SECRET}`,
-				},
+			switch (res.status) {
+				case 'pending':
+					console.log('user is paying the subscription');
+					break;
+				case 'authorized':
+					console.log('user paid subscription');
+					break;
+				case 'cancelled':
+					console.log('user cancelled the subscription');
+					break;
 			}
-		).then((res) => res.json());
-
-		console.log('subscription status', res);
-
-		switch (res.status) {
-			case 'pending':
-				console.log('user is paying the subscription');
-				break;
-			case 'authorized':
-				console.log('user paid -- upgrading plan');
-				break;
-			case 'cancelled':
-				console.log(
-					'user cancelled the subscription -- downgrading plan'
-				);
-				break;
 		}
-	}
 
-	return NextResponse.json(body, { status: 200 });
+		return NextResponse.json(body, { status: 200 });
+	} catch (error) {
+		return NextResponse.json({ error }, { status: 500 });
+	}
 };
