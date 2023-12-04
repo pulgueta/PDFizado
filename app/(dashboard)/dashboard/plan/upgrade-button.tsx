e8fccf2240e6b41a550349bded664529d9e463ec
pulgueta <retardix456@gmail.com>
2023-12-04 14:31:55 -0500

    Refactor UpgradeButton to utilize the MercadoPago API for plan upgrades
    This change refactors the UpgradeButton component in the
    dashboard/plan/upgrade-button.tsx file to use the MercadoPago API for
    handling plan upgrades. Instead of manually constructing the request
    body, it now accepts a plan parameter and sends a POST request to the
    '/api/checkout/mercadopago' endpoint with the plan information. The
    response is then used to redirect the user to the payment page. This
    refactoring improves the code by centralizing the logic for handling
    plan upgrades and makes it easier to modify or extend in the future. No
    issues are referenced in this commit.

--- app/(dashboard)/dashboard/plan/upgrade-button.tsx
+++ app/(dashboard)/dashboard/plan/upgrade-button.tsx
@@ -1,55 +1,33 @@
 'use client';
 
 import { Button } from '~/shadcn/button';
-import { env } from '~/env';
+import { Subscription } from '~/types';
 
 export const UpgradeButton = () => {
-	const body = {
-		reason: 'PDFizado - Plan estándar',
-		auto_recurring: {
-			frequency: 1,
-			frequency_type: 'months',
-			transaction_amount: 25000,
-			currency_id: 'COP',
-		},
-		back_url: `${env.BASE_URL}/dashboard/plan`,
-		payer_email: 'test_user_538458526@testuser.com',
-	};
-
-	const onStandard = async () => {
-		const res = await fetch('https://api.mercadopago.com/preapproval', {
+	const onUpgrade = (plan: string) => async () => {
+		const res = (await fetch('/api/checkout/mercadopago', {
 			method: 'POST',
-			body: JSON.stringify({ body }),
-			mode: 'cors',
-			headers: {
-				'Content-Type': 'application/json',
-				Authorization: `Bearer ${env.MERCADOPAGO_DEV_SECRET}`,
-			},
-		})
-			.then((res) => {
-				res.json();
-			})
-			.catch((err) => {
-				console.log(err);
-			});
+			body: JSON.stringify(plan),
+		}).then((res) => res.json())) as Subscription;
 
-		console.log(res);
+		window.location.href = res.init_point;
 	};
-
-	// const professional =
-	// 	'https://www.mercadopago.com.co/subscriptions/checkout?preapproval_plan_id=2c9380848c2cfc6a018c2de3ffb3005e';
 
 	return (
 		<>
-			<Button className='w-full md:w-auto' onClick={onStandard}>
+			<Button
+				className='w-full md:w-auto'
+				onClick={onUpgrade('standard')}
+			>
 				Actualizar a estándar
 			</Button>
-			{/* <Button
+
+			<Button
 				className='w-full md:w-auto'
-				onClick={() => (window.location.href = professional)}
+				onClick={onUpgrade('professional')}
 			>
 				Actualizar a profesional
-			</Button> */}
+			</Button>
 		</>
 	);
 };
