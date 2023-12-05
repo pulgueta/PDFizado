181884e19522b671da0573a06d1ce019258cf0e9
pulgueta <retardix456@gmail.com>
2023-12-04 22:26:11 -0500

    style: increased max width for better spacing for cards

--- app/(dashboard)/dashboard/pricing/page.tsx
+++ app/(dashboard)/dashboard/pricing/page.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
 const Pricing = () => {
 	return (
 		<div className='relative min-h-[calc(100vh-205px)] bg-white p-4 dark:bg-[#1C1917]'>
-			<div className='mx-auto my-8 max-w-5xl'>
+			<div className='mx-auto my-8 max-w-7xl'>
 				<h3 className='mb-4 border-b pb-4 text-center text-4xl font-black text-black dark:text-white md:mb-8'>
 					Tenemos 3 planes para que puedas utilizar{' '}
 					<span className='text-primary'>PDF</span>izado
