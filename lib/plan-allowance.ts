56f4f901458da339e685ffc15e058e73b3031852
pulgueta <retardix456@gmail.com>
2023-12-04 22:47:23 -0500

    refactor: Updated GPT models and plan allowance

--- lib/plan-allowance.ts
+++ lib/plan-allowance.ts
@@ -1,25 +1,25 @@
 export const maxSizeAllowed = (plan: string) => {
-    switch (plan) {
-        case 'FREE':
-            return 16000000;
-        case 'STANDARD':
-            return 48000000;
-        case 'PROFESSIONAL':
-            return 8000000000;
-        default:
-            return 16000000;
-    }
+	switch (plan) {
+		case 'FREE':
+			return 8000000;
+		case 'STANDARD':
+			return 32000000;
+		case 'PROFESSIONAL':
+			return 8000000000;
+		default:
+			return 16000000;
+	}
 };
 
 export const plan = (plan: string) => {
-    switch (plan) {
-        case 'FREE':
-            return 'gpt-3.5-turbo-1106';
-        case 'STANDARD':
-            return 'gpt-4-1106-preview';
-        case 'PROFESSIONAL':
-            return 'gpt-4-1106-preview';
-        default:
-            return 'gpt-3.5-turbo-1106';
-    }
+	switch (plan) {
+		case 'FREE':
+			return 'gpt-3.5-turbo-0613';
+		case 'STANDARD':
+			return 'gpt-4-32k';
+		case 'PROFESSIONAL':
+			return 'gpt-4-32k';
+		default:
+			return 'gpt-3.5-turbo-0613';
+	}
 };
