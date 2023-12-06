fdaf1832530c86f6909209dd46105128a206ee0e
pulgueta <retardix456@gmail.com>
2023-12-05 20:46:37 -0500

    Removed bottom bubble message item

--- components/client/chat/message-list.tsx
+++ components/client/chat/message-list.tsx
@@ -1,34 +1,23 @@
-import { Message } from 'ai';
-
 import { cn } from '~/lib/utils';
+import { Message } from './types';
 
 export const MessageList = ({ messages }: { messages: Message[] }) => {
-    if (!messages) return <></>;
+	if (!messages) return <></>;
 
-    return messages.map((message) => (
-        <div
-            key={message.id}
-            className={cn(
-                'relative mb-4 flex w-max animate-fade-up rounded-lg animate-duration-[400ms] animate-ease-in-out',
-                {
-                    'ml-auto  bg-primary': message.role === 'user',
-                    'mr-auto bg-neutral-600': message.role === 'assistant',
-                }
-            )}
-        >
-            <div className='relative max-w-sm rounded-lg px-4 py-2 text-white md:max-w-md lg:max-w-lg'>
-                <p className='text-sm'>{message.content}</p>
-                <div
-                    className={cn(
-                        'absolute bottom-0 h-3 w-3 rotate-45 bg-primary',
-                        {
-                            'right-1 bg-primary': message.role === 'user',
-                            'left-1 bg-neutral-600':
-                                message.role === 'assistant',
-                        }
-                    )}
-                ></div>
-            </div>
-        </div>
-    ));
+	return messages.map((message) => (
+		<div
+			key={message.id}
+			className={cn(
+				'mb-4 flex w-max animate-fade-up rounded-lg animate-duration-[400ms] animate-ease-in-out',
+				{
+					'ml-auto  bg-primary': message.isUserMessage,
+					'mr-auto bg-neutral-600': !message.isUserMessage,
+				}
+			)}
+		>
+			<div className='max-w-sm rounded-lg px-4 py-2 text-white md:max-w-md lg:max-w-lg'>
+				<p className='text-sm'>{message.text}</p>
+			</div>
+		</div>
+	));
 };
