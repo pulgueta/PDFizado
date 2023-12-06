ada2f7c5c2b66d349db7be5559700dfe5a3c1f0f
pulgueta <retardix456@gmail.com>
2023-12-05 21:14:04 -0500

    Added a new href that leads the user to the home page or dashboard depending if its logged or not

--- components/client/navbar/navbar.tsx
+++ components/client/navbar/navbar.tsx
@@ -6,12 +6,15 @@ import { ThemeSwitcher } from '~/shadcn/theme-switcher';
 import { Sheet, SheetContent, SheetTrigger } from '~/shadcn/sheet';
 import { Button } from '~/shadcn/button';
 import { DesktopRoutes, MobileRoutes } from './routes';
+import { auth } from '~/lib/auth';
 
-export const Navbar = () => {
+export const Navbar = async () => {
+	const session = await auth();
+
 	return (
 		<header className='sticky top-0 z-50 flex h-20 items-center justify-between border-b bg-white/80 px-4 backdrop-blur dark:bg-[#0C0A09]/80 md:px-6 lg:px-16 xl:px-40 2xl:px-64'>
 			<Link
-				href='/'
+				href={!session ? '/' : '/dashboard'}
 				aria-label='PDFizado - Inicio'
 				className='text-3xl font-black text-black dark:text-white md:text-4xl'
 			>
