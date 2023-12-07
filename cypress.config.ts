daae8fbff1f02212efe6452d79d39f249b5223e4
pulgueta <retardix456@gmail.com>
2023-12-07 17:14:14 -0500

    Added envs for Cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-	e2e: {},
-	env: {
-		BASE_URL: 'http://localhost:3000',
+	e2e: {
+		baseUrl: 'http://localhost:3000',
+		env: {
+			BASE_URL: process.env.BASE_URL ?? '',
+			CY_EMAIL: process.env.CY_EMAIL ?? '',
+			CY_PASSWORD: process.env.CY_PASSWORD ?? '',
+		},
 	},
+	env: {
+		BASE_URL: process.env.BASE_URL ?? '',
+		CY_EMAIL: process.env.CY_EMAIL ?? '',
+		CY_PASSWORD: process.env.CY_PASSWORD ?? '',
+	},
+	viewportHeight: 932,
+	viewportWidth: 430,
 });
