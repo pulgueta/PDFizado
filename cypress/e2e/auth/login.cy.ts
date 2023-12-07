c9baf9d8944c4506d364935a980b96efb2db9524
pulgueta <retardix456@gmail.com>
2023-12-07 17:46:54 -0500

    Modified the file to type the data inside the fixture

--- cypress/e2e/auth/login.cy.ts
+++ cypress/e2e/auth/login.cy.ts
@@ -1,4 +1,6 @@
-describe('Auth', () => {
+import { User } from '~/cypress/types';
+
+describe('Login', () => {
 	it('Visit the Login page and login with valid credentials', () => {
 		cy.visit(Cypress.env('BASE_URL'));
 
@@ -6,39 +8,24 @@ describe('Auth', () => {
 		cy.wait(1000);
 		cy.get('#login').click({ force: true });
 		cy.wait(1000);
-		cy.get('#email').type('retardix456@gmail.com');
-		cy.wait(1000);
-		cy.get('#password').type('olakase123');
-		cy.wait(1000);
-		cy.get('#submit-btn').click();
-		cy.wait(1000);
-		cy.get('#dashboard-title')
-			.should('exist')
-			.should('contain.text', 'Dashboard de');
-	});
-	it('Login and logout', () => {
-		cy.visit(Cypress.env('BASE_URL'));
-
-		cy.get('#navbar-btn').click();
-		cy.wait(1000);
-		cy.get('#login').click({ force: true });
-		cy.wait(1000);
-		cy.get('#email').type('retardix456@gmail.com');
-		cy.wait(1000);
-		cy.get('#password').type('olakase123');
-		cy.wait(1000);
-		cy.get('#submit-btn').click();
-		cy.wait(1000);
-		cy.get('#dashboard-title')
-			.should('exist')
-			.should('contain.text', 'Dashboard de');
-		cy.wait(1000);
-		cy.get('#navbar-btn').click();
-		cy.wait(1000);
-		cy.get('#logout-btn').click({ force: true });
-		cy.wait(2000);
-		cy.get('#branding').click({ force: true });
-		cy.wait(1000);
-		cy.get('#landing-title').should('contain.text', 'PDFizado');
+		cy.fixture('user.json').then((user: User) => {
+			cy.get('#email').type(user.email);
+			cy.wait(1000);
+			cy.get('#password').type(user.password);
+			cy.wait(1000);
+			cy.get('#submit-btn').click();
+			cy.wait(1000);
+			cy.get('#dashboard-title')
+				.should('exist')
+				.should('contain.text', 'Dashboard de');
+			cy.wait(1000);
+			cy.get('#navbar-btn').click();
+			cy.wait(1000);
+			cy.get('#logout-btn').click({ force: true });
+			cy.wait(2000);
+			cy.get('#branding').click({ force: true });
+			cy.wait(1000);
+			cy.get('#landing-title').should('contain.text', 'PDFizado');
+		});
 	});
 });
