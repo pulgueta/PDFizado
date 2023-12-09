44561a06f44425cfd41275a92ec48e21bcfbb63c
pulgueta <retardix456@gmail.com>
2023-12-08 23:21:45 -0500

    Moved the BASE_URL to top

--- env/server.mjs
+++ env/server.mjs
@@ -3,6 +3,7 @@ import { ZodError, z } from 'zod';
 
 export const env = createEnv({
 	server: {
+		BASE_URL: z.string().url(),
 		DATABASE_URL: z.string().url(),
 		AUTH_URL: z.string().url(),
 		AUTH_SECRET: z.string().min(8),
@@ -14,9 +15,9 @@ export const env = createEnv({
 		OPENAI_SECRET: z.string().min(8),
 		OPENAI_ORG: z.string().min(8),
 		RESEND_API_KEY: z.string().min(8),
-		BASE_URL: z.string().url()
 	},
 	runtimeEnv: {
+		BASE_URL: process.env.BASE_URL,
 		DATABASE_URL: process.env.DATABASE_URL,
 		AUTH_URL: process.env.AUTH_URL,
 		AUTH_SECRET: process.env.AUTH_SECRET,
@@ -28,7 +29,6 @@ export const env = createEnv({
 		OPENAI_SECRET: process.env.OPENAI_SECRET,
 		OPENAI_ORG: process.env.OPENAI_ORG,
 		RESEND_API_KEY: process.env.RESEND_API_KEY,
-		BASE_URL: process.env.BASE_URL,
 	},
 	onValidationError: (error = ZodError) => {
 		console.error(
