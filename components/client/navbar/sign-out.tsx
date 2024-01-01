fb952292f28a474c981b1a7a7cb4804d70352759
pulgueta <retardix456@gmail.com>
2023-12-31 19:10:48 -0500

    Updated logout button and server action

--- components/client/navbar/sign-out.tsx
+++ components/client/navbar/sign-out.tsx
@@ -1,27 +1,45 @@
+'use client';
+
+import { useTransition } from 'react';
+
 import { Button } from '~/shadcn/button';
 import { logout } from './action';
 
-export const SignOut = () => (
-	<form action={logout}>
-		<Button
-			aria-label='Cerrar sesión'
-			id='logout-btn'
-			variant='destructive'
-		>
-			Cerrar sesión
-		</Button>
-	</form>
-);
+export const SignOut = () => {
+	const [isPending, startTransition] = useTransition();
 
-export const SignOutMobile = () => (
-	<form action={logout}>
-		<Button
-			className='w-full font-medium'
-			aria-label='Cerrar sesión'
-			id='logout-btn'
-			variant='destructive'
-		>
-			Cerrar sesión
-		</Button>
-	</form>
-);
+	const onLogout = () => startTransition(async () => logout());
+
+	return (
+		<form action={onLogout}>
+			<Button
+				aria-label={isPending ? 'Cerrando sesión...' : 'Cerrar sesión'}
+				id='logout-btn'
+				variant='destructive'
+				disabled={isPending}
+			>
+				{isPending ? 'Cerrando sesión...' : 'Cerrar sesión'}
+			</Button>
+		</form>
+	);
+};
+
+export const SignOutMobile = () => {
+	const [isPending, startTransition] = useTransition();
+
+	const onLogout = () => startTransition(async () => logout());
+
+	return (
+		<form action={onLogout}>
+			<Button
+				className='w-full font-medium'
+				aria-label={isPending ? 'Cerrando sesión...' : 'Cerrar sesión'}
+				id='logout-btn'
+				variant='destructive'
+				disabled={isPending}
+			>
+				{isPending ? 'Cerrando sesión...' : 'Cerrar sesión'}
+			</Button>
+		</form>
+	);
+};
