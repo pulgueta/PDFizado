6edd7720c7a00f1ee63913f00b8c75ee3bd53ee6
pulgueta <retardix456@gmail.com>
2023-12-31 19:05:53 -0500

    Updated logout server action

--- components/client/navbar/action.ts
+++ components/client/navbar/action.ts
@@ -1,6 +1,10 @@
 'use server';
 
+import { revalidatePath } from 'next/cache';
+
 import { signOut } from '~/lib/auth';
 
-export const logout = async () =>
+export const logout = async () => {
 	await signOut({ redirectTo: '/login', redirect: true });
+	revalidatePath('/');
+};
