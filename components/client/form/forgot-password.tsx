1815048ff53a8a4aa46e7866061ad2ce78257eed
pulgueta <retardix456@gmail.com>
2024-01-04 23:12:37 -0500

    Updated form to use native react hooks for <form>

--- components/client/form/forgot-password.tsx
+++ components/client/form/forgot-password.tsx
@@ -1,79 +1,44 @@
 'use client';
 
-import { toast } from 'sonner';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { Loader2Icon } from 'lucide-react';
-import { useForm } from 'react-hook-form';
+import { ElementRef, useEffect, useRef } from 'react';
+import { useFormState, useFormStatus } from 'react-dom';
 
-import {
-	Form,
-	FormControl,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
-} from '~/shadcn/form';
-import { Button } from '~/shadcn/button';
+import Link from 'next/link';
+
+import { toast } from 'sonner';
+import { Loader2Icon } from 'lucide-react';
+
+import { Button, buttonVariants } from '~/shadcn/button';
 import {
 	Card,
 	CardContent,
 	CardDescription,
+	CardFooter,
 	CardHeader,
 	CardTitle,
 } from '~/shadcn/card';
 import { Input } from '~/shadcn/input';
-import { type ForgotPassword, forgotPasswordSchema } from '~/schemas';
+import { Label } from '~/shadcn/label';
+import { Separator } from '~/shadcn/separator';
+import { forgotPassword } from './actions/password';
 
 export const ForgotPasswordForm = () => {
-	const form = useForm<ForgotPassword>({
-		resolver: zodResolver(forgotPasswordSchema),
-		defaultValues: {
-			email: '',
-		},
-	});
+	const [state, action] = useFormState(forgotPassword, undefined);
+	const form = useRef<ElementRef<'form'>>(null);
 
-	const onSubmit = form.handleSubmit(async ({ email }: ForgotPassword) => {
-		const res = await fetch('/api/email/forgot-password', {
-			method: 'POST',
-			body: JSON.stringify({
-				email,
-			}),
-		});
+	useEffect(() => {
+		if (state?.status === 'success') {
+			toast.success(
+				'Te hemos enviado un correo, podría tardar unos minutos'
+			);
 
-		if (!res.ok) {
-			switch (res.status) {
-				case 404:
-					toast.error('No existe el usuario', {
-						description:
-							'No hay un usuario registrado con ese correo electrónico.',
-					});
-
-					return;
-				case 401:
-					toast.error('Usuario no verificado', {
-						description:
-							'Debes verificar tu correo electrónico para poder cambiar tu contraseña.',
-					});
-
-					return;
-				case 500:
-					toast.error('Error', {
-						description:
-							'Ocurrió un error al enviar el correo de recuperación.',
-					});
-
-					return;
-			}
+			form.current?.reset();
 		}
 
-		toast.success('Correo enviado', {
-			description:
-				'Se ha enviado el correo de recuperación, revisa tu bandeja de entrada.',
-		});
-
-		form.clearErrors();
-		form.reset();
-	});
+		if (state?.error) {
+			toast.error(`${state.error}`);
+		}
+	}, [state]);
 
 	return (
 		<Card className='max-w-md'>
@@ -85,46 +50,51 @@ export const ForgotPasswordForm = () => {
 				</CardDescription>
 			</CardHeader>
 			<CardContent>
-				<Form {...form}>
-					<form onSubmit={onSubmit} className='space-y-6'>
-						<FormField
-							control={form.control}
-							name='email'
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Correo electrónico</FormLabel>
-									<FormControl>
-										<Input
-											autoComplete='Correo'
-											placeholder='Tu correo registrado'
-											type='email'
-											id='email'
-											disabled={
-												form.formState.isSubmitting
-											}
-											{...field}
-										/>
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-
-						<Button
-							type='submit'
-							className='w-full'
-							id='submit-btn'
-							disabled={form.formState.isSubmitting}
-						>
-							{form.formState.isSubmitting ? (
-								<Loader2Icon className='h-4 w-4 animate-spin' />
-							) : (
-								'Recuperar contraseña'
-							)}
-						</Button>
-					</form>
-				</Form>
+				<form action={action} ref={form}>
+					<Label>Correo electrónico</Label>
+					<Input
+						autoComplete='Correo'
+						placeholder='Tu correo registrado'
+						type='email'
+						id='email'
+						name='email'
+						className='mb-4 mt-2'
+					/>
+					<Submit />
+				</form>
 			</CardContent>
+			<CardFooter className='flex-col justify-center'>
+				<Separator />
+				<Link
+					href='/forgot-password'
+					className={buttonVariants({
+						variant: 'link',
+						size: 'sm',
+						className: 'mt-4',
+					})}
+				>
+					Volver a iniciar sesión
+				</Link>
+			</CardFooter>
 		</Card>
+	);
+};
+
+const Submit = () => {
+	const { pending } = useFormStatus();
+
+	return (
+		<Button
+			type='submit'
+			className='w-full'
+			id='submit-btn'
+			disabled={pending}
+		>
+			{pending ? (
+				<Loader2Icon className='animate-spin' />
+			) : (
+				'Recuperar contraseña'
+			)}
+		</Button>
 	);
 };
