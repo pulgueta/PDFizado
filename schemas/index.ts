e420d97bcc4433744e88912547690aea729324b7
pulgueta <retardix456@gmail.com>
2024-01-05 12:54:00 -0500

    Refactor schema creation

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,74 +1,72 @@
-import { TypeOf, z } from 'zod';
+import { TypeOf, object, string } from 'zod';
 
-export const loginSchema = z.object({
-	email: z
-		.string()
-		.email({ message: 'Debes ingresar un correo electrónico válido' }),
-	password: z
-		.string()
-		.min(4, 'La contraseña debe ser de al menos 4 caracteres.'),
+export const loginSchema = object({
+	email: string().email({
+		message: 'Debes ingresar un correo electrónico válido',
+	}),
+	password: string().min(
+		4,
+		'La contraseña debe ser de al menos 4 caracteres.'
+	),
 });
 
 export type Login = TypeOf<typeof loginSchema>;
 
-export const registerSchema = z
-	.object({
-		name: z.string().min(6, 'El nombre debe tener al menos 6 caracteres.'),
-		email: z
-			.string()
-			.min(6, 'El correo electrónico debe tener al menos 6 caracteres.')
-			.email({ message: 'Debes ingresar un correo electrónico válido' }),
-		password: z
-			.string()
-			.min(4, 'La contraseña debe ser de al menos 6 caracteres.'),
-		confirmPassword: z
-			.string()
-			.min(4, 'La contraseña debe ser de al menos 6 caracteres.'),
-	})
-	.refine(({ password, confirmPassword }) => password === confirmPassword, {
-		message: 'Las contraseñas deben coincidir',
-		path: ['confirmPassword'],
-	});
+export const registerSchema = object({
+	name: string().min(6, 'El nombre debe tener al menos 6 caracteres.'),
+	email: string()
+		.min(6, 'El correo electrónico debe tener al menos 6 caracteres.')
+		.email({ message: 'Debes ingresar un correo electrónico válido' }),
+	password: string().min(
+		4,
+		'La contraseña debe ser de al menos 6 caracteres.'
+	),
+	confirmPassword: string().min(
+		4,
+		'La contraseña debe ser de al menos 6 caracteres.'
+	),
+}).refine(({ password, confirmPassword }) => password === confirmPassword, {
+	message: 'Las contraseñas deben coincidir',
+	path: ['confirmPassword'],
+});
 
 export type Register = TypeOf<typeof registerSchema>;
 
-export const awsSchema = z.object({
-	key: z.string().min(1, 'La llave debe tener al menos 1 caracter.'),
-	name: z.string().min(1, 'El nombre debe tener al menos 1 caracter.'),
-	url: z.string().url({ message: 'Debes ingresar una url válida' }),
+export const awsSchema = object({
+	key: string().min(1, 'La llave debe tener al menos 1 caracter.'),
+	name: string().min(1, 'El nombre debe tener al menos 1 caracter.'),
+	url: string().url({ message: 'Debes ingresar una url válida' }),
 });
 
-export const emailSchema = z.object({
-	email: z
-		.string()
-		.email({ message: 'Debes ingresar un correo electrónico válido' }),
+export const emailSchema = object({
+	email: string().email({
+		message: 'Debes ingresar un correo electrónico válido',
+	}),
 });
 
-export const forgotPasswordSchema = z.object({
-	email: z
-		.string()
+export const forgotPasswordSchema = object({
+	email: string()
 		.min(6, 'El correo electrónico debe tener al menos 6 caracteres')
 		.email({ message: 'Debes ingresar un correo electrónico válido' }),
 });
 
 export type ForgotPassword = TypeOf<typeof forgotPasswordSchema>;
 
-export const resetSchema = z
-	.object({
-		email: z
-			.string()
-			.min(6, 'El correo electrónico debe tener al menos 6 caracteres')
-			.email({ message: 'Debes ingresar un correo electrónico válido' }),
-		password: z
-			.string()
-			.min(6, 'La contraseña debe ser de al menos 6 caracteres.'),
-		confirmPassword: z
-			.string()
-			.min(6, 'La contraseña debe ser de al menos 6 caracteres.'),
-	})
-	.refine(({ password, confirmPassword }) => password === confirmPassword, {
-		message: 'Las contraseñas deben coincidir',
-		path: ['confirmPassword'],
-	});
+export const resetSchema = object({
+	email: string()
+		.min(6, 'El correo electrónico debe tener al menos 6 caracteres')
+		.email({ message: 'Debes ingresar un correo electrónico válido' }),
+	password: string().min(
+		6,
+		'La contraseña debe ser de al menos 6 caracteres.'
+	),
+	confirmPassword: string().min(
+		6,
+		'La contraseña debe ser de al menos 6 caracteres.'
+	),
+}).refine(({ password, confirmPassword }) => password === confirmPassword, {
+	message: 'Las contraseñas deben coincidir',
+	path: ['confirmPassword'],
+});
 
 export type ResetPassword = TypeOf<typeof resetSchema>;
