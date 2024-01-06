77397c28c600d1972b46595c4831dbe021b0efc0
pulgueta <retardix456@gmail.com>
2024-01-06 01:59:05 -0500

    Refactor conditional render and type

--- components/client/chat/message-list.tsx
+++ components/client/chat/message-list.tsx
@@ -1,8 +1,20 @@
+import { Message } from 'ai/react';
+import { ChatCompletionRequestMessageRoleEnum } from 'openai-edge';
+
 import { cn } from '~/lib/utils';
-import { Message } from './types';
 
 export const MessageList = ({ messages }: { messages: Message[] }) => {
 	if (!messages) return <></>;
+
+	// if (loading)
+	// 	return (
+	// 		<div
+	// 			key={`${messages[0].id}`}
+	// 			className='w-max animate-fade-up rounded-lg bg-neutral-600 px-4 py-2 animate-duration-300 md:max-w-md lg:max-w-lg'
+	// 		>
+	// 			<Loader2 className='size-4 animate-spin' />
+	// 		</div>
+	// 	);
 
 	return messages.map((message) => (
 		<div
@@ -10,13 +22,17 @@ export const MessageList = ({ messages }: { messages: Message[] }) => {
 			className={cn(
 				'mb-4 flex w-max animate-fade-up rounded-lg animate-duration-[400ms] animate-ease-in-out',
 				{
-					'ml-auto  bg-primary': message.isUserMessage,
-					'mr-auto bg-neutral-600': !message.isUserMessage,
+					'ml-auto  bg-primary':
+						message.role ===
+						ChatCompletionRequestMessageRoleEnum.User,
+					'mr-auto bg-neutral-600':
+						message.role !==
+						ChatCompletionRequestMessageRoleEnum.Assistant,
 				}
 			)}
 		>
 			<div className='max-w-sm rounded-lg px-4 py-2 text-white md:max-w-md lg:max-w-lg'>
-				<p className='text-sm'>{message.text}</p>
+				<p className='text-sm'>{message.content}</p>
 			</div>
 		</div>
 	));
