7ae5a61f9f5451f21f86edba323b7e550c3db1c0
pulgueta <retardix456@gmail.com>
2024-01-06 02:03:42 -0500

    Moved routes file location

---
+++ components/server/navbar/routes.tsx
@@ -0,0 +1,48 @@
+import Link from 'next/link';
+
+import { SheetTrigger } from '~/shadcn/sheet';
+import { noAuthRoutes } from '~/constants/navbar';
+import { buttonVariants } from '~/components/ui/button';
+
+export const DesktopRoutes = () => {
+	return (
+		<ul className='hidden md:flex md:items-center md:gap-x-6'>
+			{noAuthRoutes.map(({ href, label }) => (
+				<li key={href}>
+					<Link
+						href={href}
+						id={href.replace('/', '')}
+						aria-label={label}
+						className={buttonVariants({
+							variant: 'ghost',
+							className: 'text-lg',
+						})}
+					>
+						{label}
+					</Link>
+				</li>
+			))}
+		</ul>
+	);
+};
+
+export const MobileRoutes = () => {
+	return (
+		<ul className='space-y-4'>
+			{noAuthRoutes.map(({ href, label }) => (
+				<li key={href}>
+					<SheetTrigger asChild>
+						<Link
+							href={href}
+							aria-label={label}
+							id={href.replace('/', '')}
+							className='text-center text-lg font-semibold leading-loose duration-200 ease-in-out hover:text-primary'
+						>
+							{label}
+						</Link>
+					</SheetTrigger>
+				</li>
+			))}
+		</ul>
+	);
+};
