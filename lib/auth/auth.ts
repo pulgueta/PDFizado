3d72c290197f84d51f70bd6e4bf9af0cf1902502
pulgueta <retardix456@gmail.com>
2024-01-06 15:01:43 -0500

    Added user plan to session

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -88,6 +88,7 @@ export const {
 				session.user.name = token.name;
 				session.user.email = token.email;
 				session.user.isOAuth = token.isOAuth as boolean;
+				session.user.plan = token.plan as Plan;
 			}
 
 			return session;
