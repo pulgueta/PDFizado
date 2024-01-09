4d3971674e8ef36c3fa326d369b127f262f8ec2b
pulgueta <retardix456@gmail.com>
2024-01-08 22:17:14 -0500

    Update image remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,25 @@ import './env/server.mjs'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	poweredByHeader: false,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'd1fwmfi01nicf2.cloudfront.net',
+				port: '',
+				pathname: '/pictures/**',
+			},
+			{
+				hostname: 'via.placeholder.com', protocol: 'https'
+			},
+			{
+				hostname: 'lh3.googleusercontent.com',
+				pathname: '/a/**',
+				protocol: 'https',
+				port: ''
+			}
+		],
+	},
 	experimental: {
 		serverComponentsExternalPackages: ['argon2'],
 	},
