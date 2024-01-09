2da905a598ba2d34d52c14260dcd2303dfbf852d
pulgueta <retardix456@gmail.com>
2024-01-08 22:21:41 -0500

    Support form to be defined

---
+++ app/(dashboard)/dashboard/settings/@help/page.tsx
@@ -0,0 +1,24 @@
+import { Card, CardContent } from '~/shadcn/card';
+
+import { CardHeader } from '~/components/server/settings/card-header';
+import { currentUser } from '~/lib/auth/currentUser';
+
+const Plan = async () => {
+	const user = await currentUser();
+
+	if (!user) return;
+
+	return (
+		<Card className='container max-w-md px-0'>
+			<CardHeader
+				title='Soporte al usuario'
+				description='Si presentas problemas con tu plan actual o con PDFizado, por favor, rellena el siguiente formulario.'
+			/>
+
+			<CardContent>
+				<p>To be defined</p>
+			</CardContent>
+		</Card>
+	);
+};
+export default Plan;
