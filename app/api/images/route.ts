e0d759c5b8f0f179191e2f0f989c24737aca4412
pulgueta <retardix456@gmail.com>
2024-01-08 22:22:46 -0500

    Added route handler to update user's photo

---
+++ app/api/images/route.ts
@@ -0,0 +1,35 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { currentUser } from '~/lib/auth/currentUser';
+import { awsSchema } from '~/schemas';
+import { db } from '~/database/db';
+import { update } from '~/lib/auth/auth';
+import { env } from '~/env/server.mjs';
+
+export const POST = async (req: NextRequest) => {
+	const user = await currentUser();
+
+	if (!user) return NextResponse.json('Unauthorized', { status: 401 });
+
+	const body = await req.json();
+	const validatedBody = awsSchema.safeParse(body);
+
+	if (!validatedBody.success)
+		return NextResponse.json(validatedBody.error.errors, { status: 400 });
+
+	const { key } = validatedBody.data;
+
+	const { image } = await db.user.update({
+		data: {
+			image: `${env.CLOUDFRONT_HOST}${key}`,
+		},
+		where: {
+			email: user.email!,
+			id: user.id,
+		},
+	});
+
+	await update({ user: { image } });
+
+	return NextResponse.json({ image });
+};
