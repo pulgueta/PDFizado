cd4edb5bdec16114c46148ffb9e364e1f888a687
pulgueta <retardix456@gmail.com>
2024-01-20 16:58:48 -0500

    Refactored pdf card component

--- components/client/user/pdf-card.tsx
+++ components/client/user/pdf-card.tsx
@@ -1,22 +1,15 @@
+import { FC } from 'react';
+
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 
 import { File } from '@prisma/client';
-import { Loader2Icon } from 'lucide-react';
 
-import { Button, buttonVariants } from '~/shadcn/button';
+import { buttonVariants } from '~/shadcn/button';
 import { Separator } from '~/shadcn/separator';
 import { cn } from '~/lib/utils';
-import { useDeletePDF } from '~/hooks/user/use-delete-pdf';
+import { DeletePDF } from './delete-pdf-button';
 
-export const PDFCard: React.FC<File> = (file) => {
-	const pathname = usePathname();
-
-	const {
-		mutation: { isPending },
-		onDeleteFile,
-	} = useDeletePDF({ file });
-
+export const PDFCard: FC<File> = (file) => {
 	const datetime = new Date(file.createdAt).toLocaleDateString('es-ES', {
 		dateStyle: 'short',
 	});
@@ -28,7 +21,7 @@ export const PDFCard: React.FC<File> = (file) => {
 	});
 
 	return (
-		<article className='mx-auto flex w-full flex-col rounded-2xl border bg-white p-4 shadow sm:w-96 md:w-96 dark:bg-neutral-900'>
+		<article className='mx-auto flex w-full flex-col rounded-xl border bg-secondary p-4 shadow sm:w-96 md:w-96 dark:bg-popover'>
 			<header>
 				<h3 className='truncate text-xl font-bold'>{file.name}</h3>
 				<p className='text-muted-foreground'>
@@ -39,28 +32,16 @@ export const PDFCard: React.FC<File> = (file) => {
 			<Separator className='my-4' />
 			<footer className='flex w-full items-center justify-between'>
 				<Link
-					href={`${pathname}/${file.id}`}
+					href={`/dashboard/${file.id}`}
 					className={cn(
 						buttonVariants({
 							variant: 'ghost',
-						}),
-						isPending && 'cursor-not-allowed'
+						})
 					)}
 				>
 					Ver PDF
 				</Link>
-				<Button
-					variant='destructive'
-					disabled={isPending}
-					onClick={onDeleteFile(file.id)}
-					size={isPending ? 'icon' : 'default'}
-				>
-					{isPending ? (
-						<Loader2Icon className='size-4 animate-spin' />
-					) : (
-						'Eliminar PDF'
-					)}
-				</Button>
+				<DeletePDF file={file} />
 			</footer>
 		</article>
 	);
