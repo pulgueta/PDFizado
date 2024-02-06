5d236d9a11f3a19bd801123992f2a4740780747a
pulgueta <retardix456@gmail.com>
2024-02-06 06:24:24 -0500

    Update plan footer content

--- app/(dashboard)/dashboard/settings/@plan/page.tsx
+++ app/(dashboard)/dashboard/settings/@plan/page.tsx
@@ -1,10 +1,8 @@
-import Link from 'next/link';
-
 import { Card, CardContent, CardFooter } from '~/shadcn/card';
-import { CurrentPlan } from './_components/current-plan';
-import { UpgradeButton } from './_components/upgrade-button';
+import { CurrentPlan } from './components/current-plan';
 import { CardHeader } from '~/components/server/settings/card-header';
 import { currentUser } from '~/lib/auth/currentUser';
+import { PayButton } from './components/pay-button';
 
 const Plan = async () => {
 	const user = await currentUser();
@@ -12,7 +10,7 @@ const Plan = async () => {
 	if (!user) return;
 
 	return (
-		<Card className='container max-w-md px-0'>
+		<Card className='container max-w-lg px-0'>
 			<CardHeader
 				title='Plan'
 				description='Manten tu plan actual o mejóralo para obtener beneficios.'
@@ -24,21 +22,7 @@ const Plan = async () => {
 				</p>
 			</CardContent>
 			<CardFooter className='flex flex-col items-center justify-between gap-4'>
-				{user.plan === 'FREE' ? (
-					<UpgradeButton />
-				) : (
-					<span className='text-sm font-normal leading-tight text-muted-foreground'>
-						Si quieres cancelar o modificar tu suscripción actual,
-						deberás manejarla en{' '}
-						<Link
-							target='_blank'
-							href='https://www.mercadopago.com.co/subscriptions'
-							className='font-medium text-primary'
-						>
-							MercadoPago
-						</Link>
-					</span>
-				)}
+				{user.plan === 'FREE' && <PayButton />}
 			</CardFooter>
 		</Card>
 	);
