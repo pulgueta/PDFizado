243b4ff70c5d58deefd98b533e73d5e6544732be
pulgueta <retardix456@gmail.com>
2024-02-06 06:24:42 -0500

    Update dashboard pricing page

--- app/(dashboard)/dashboard/pricing/page.tsx
+++ app/(dashboard)/dashboard/pricing/page.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from 'next';
 
-import { PriceCards } from '~/components/client/prices/card-details';
+import { getSubscriptions } from '~/lib/products/get-plans';
+import { currentUser } from '~/lib/auth/currentUser';
+import { PriceCard } from '~/components/client/prices/card-details';
 
 export const metadata: Metadata = {
 	metadataBase: new URL('https://pdfizado.com/dashboard/pricing'),
@@ -9,17 +11,31 @@ export const metadata: Metadata = {
 	},
 };
 
-const Pricing = () => (
-	<div className='relative min-h-[calc(100dvh-205px)] bg-white p-4 dark:bg-[#1C1917]'>
-		<div className='mx-auto my-8 max-w-7xl'>
-			<h3 className='mb-4 border-b pb-4 text-center text-4xl font-black text-black md:mb-8 dark:text-white'>
+const Pricing = async () => {
+	const plansPromise = getSubscriptions();
+	const userPromise = currentUser();
+
+	const [user, plans] = await Promise.all([userPromise, plansPromise]);
+
+	return (
+		<div className='relative min-h-[calc(100dvh-205px)] bg-white p-4 dark:bg-[#1C1917]'>
+			<h3 className='mb-8 text-balance text-center text-4xl font-black tracking-tight text-black dark:text-white md:mb-16 md:text-5xl'>
 				Tenemos 3 planes para que puedas utilizar{' '}
 				<span className='text-primary'>PDF</span>izado
 			</h3>
-
-			<PriceCards />
+			<div className='container'>
+				<div className='grid grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-3'>
+					{plans.map((plan) => (
+						<PriceCard
+							{...plan}
+							email={user?.email!}
+							key={plan.id}
+						/>
+					))}
+				</div>
+			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 export default Pricing;
