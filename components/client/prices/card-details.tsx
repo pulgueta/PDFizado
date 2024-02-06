b5ef82be87fc8a1c31a626490e1fc40398785d72
pulgueta <retardix456@gmail.com>
2024-02-06 06:21:31 -0500

    Updated pricing cards

--- components/client/prices/card-details.tsx
+++ components/client/prices/card-details.tsx
@@ -1,174 +1,90 @@
+import { UrlObject } from 'url';
+
+import { FC } from 'react';
+
 import Link from 'next/link';
 
-import { HelpCircleIcon } from 'lucide-react';
+import { Plans } from '@prisma/client';
+import { CheckIcon } from 'lucide-react';
 
 import {
 	Card,
 	CardContent,
 	CardDescription,
-	CardFooter,
 	CardHeader,
 	CardTitle,
 } from '~/shadcn/card';
-import {
-	Tooltip,
-	TooltipContent,
-	TooltipProvider,
-	TooltipTrigger,
-} from '~/shadcn/tooltip';
 import { Separator } from '~/shadcn/separator';
 import { buttonVariants } from '~/shadcn/button';
 
-export const PriceCards = () => {
-	const standard = new Intl.NumberFormat('es-CO', {
-		style: 'currency',
-		currency: 'COP',
-	})
-		.format(25000)
-		.replace(',00', '');
+type PriceCardMetadata = Plans & {
+	email: string;
+};
 
-	const professional = new Intl.NumberFormat('es-CO', {
+export const PriceCard: FC<PriceCardMetadata> = ({
+	benefits,
+	description,
+	email,
+	id,
+	lemonSqueezyHref,
+	monthlyPrice,
+	name,
+}) => {
+	const formattedPrice = new Intl.NumberFormat('es-CO', {
 		style: 'currency',
 		currency: 'COP',
-	})
-		.format(50000)
-		.replace(',00', '');
+	});
+
+	const href =
+		lemonSqueezyHref === ''
+			? '/dashboard/settings'
+			: `${lemonSqueezyHref}?checkout[email]=${email}`;
+
+	const url = email !== undefined ? href : '/login';
 
 	return (
-		<>
-			<p className='mb-8 text-center text-muted-foreground'>
-				*Precio en COP
-			</p>
-			<TooltipProvider>
-				<div className='mx-auto flex grow flex-col gap-4 lg:flex-row'>
-					<Card className='mx-auto w-full bg-neutral-50 sm:w-96 md:w-[25rem] dark:bg-[#131110]'>
-						<CardHeader className='text-center'>
-							<CardTitle className='text-3xl font-bold'>
-								Gratis
-							</CardTitle>
-							<CardDescription>
-								Para documentos simples
-							</CardDescription>
-						</CardHeader>
-						<CardContent>
-							<h4 className='text-center text-5xl font-black'>
-								Sin costo
-							</h4>
-							<Separator className='my-4' />
-							<ul className='text-muted-foreground'>
-								<li>&#9989; 8MB de tamaño límite</li>
-								<li>&#9989; Respuesta estándar</li>
-								<li>&#9989; Soporte estándar</li>
-								<li>&#9989; Subir hasta 6 PDFs</li>
-								<li>&#10060; Respuestas más claras</li>
-								<li>&#10060; Soporte prioritario</li>
-								<li>
-									&#10060; Alojamiento de PDFs de por vida
-									<Tooltip delayDuration={100}>
-										<TooltipTrigger aria-label='Más información'>
-											<HelpCircleIcon className='ml-2 h-4 w-4' />
-										</TooltipTrigger>
-										<TooltipContent>
-											<p>
-												Se eliminarán los PDFs en 60
-												días
-											</p>
-										</TooltipContent>
-									</Tooltip>
-								</li>
-							</ul>
-						</CardContent>
-						<CardFooter>
-							<Link
-								href='/dashboard/settings'
-								className={buttonVariants({
-									className: 'w-full',
-								})}
-							>
-								Adquirir
-							</Link>
-						</CardFooter>
-					</Card>
-					<Card className='mx-auto w-full bg-neutral-50 sm:w-96 md:w-[25rem] dark:bg-[#131110]'>
-						<CardHeader className='text-center'>
-							<CardTitle className='text-3xl font-bold'>
-								Estándar
-							</CardTitle>
-							<CardDescription>
-								Para documentos más grandes
-							</CardDescription>
-						</CardHeader>
-						<CardContent>
-							<h4 className='text-center text-5xl font-black'>
-								{standard}
-							</h4>
-							<Separator className='my-4' />
-							<ul className='text-muted-foreground'>
-								<li>&#9989; 32MB de tamaño límite</li>
-								<li>&#9989; Respuesta más rápida</li>
-								<li>&#9989; Subir hasta 12 PDFs</li>
-								<li>&#9989; Respuestas más claras</li>
-								<li>&#9989; Soporte prioritario</li>
-								<li>&#9989; Alojamiento de PDFs de por vida</li>
-								<li>
-									&#10060; Acceso a funciones futuras con
-									anticipación
-								</li>
-							</ul>
-						</CardContent>
-						<CardFooter>
-							<Link
-								href='/dashboard/settings'
-								className={buttonVariants({
-									className: 'w-full',
-								})}
-							>
-								Adquirir
-							</Link>
-						</CardFooter>
-					</Card>
-					<Card className='mx-auto w-full bg-neutral-50 sm:w-96 md:w-[25rem] dark:bg-[#131110]'>
-						<CardHeader className='text-center'>
-							<CardTitle className='text-3xl font-bold'>
-								Profesional
-							</CardTitle>
-							<CardDescription>
-								Para cualquier documento extenso
-							</CardDescription>
-						</CardHeader>
-						<CardContent>
-							<h4 className='text-center text-5xl font-black'>
-								{professional}
-							</h4>
-							<Separator className='my-4' />
-							<ul className='text-muted-foreground'>
-								<li>&#9989; Sin límite de tamaño</li>
-								<li>&#9989; Respuesta más rápida</li>
-								<li>&#9989; PDFs ilimitados</li>
-								<li>&#9989; Respuestas más claras</li>
-								<li>
-									&#9989; Soporte prioritario y personalizado
-								</li>
-								<li>&#9989; Alojamiento de PDFs de por vida</li>
-								<li>
-									&#9989; Acceso a funciones futuras con
-									anticipación
-								</li>
-							</ul>
-						</CardContent>
-						<CardFooter>
-							<Link
-								href='/dashboard/settings'
-								className={buttonVariants({
-									className: 'w-full',
-								})}
-							>
-								Adquirir
-							</Link>
-						</CardFooter>
-					</Card>
-				</div>
-			</TooltipProvider>
-		</>
+		<Card
+			className='mx-auto w-full bg-neutral-50 dark:bg-[#131110] sm:w-96 md:w-[25rem]'
+			key={id}
+		>
+			<CardHeader>
+				<CardTitle className='text-4xl font-bold'>{name}</CardTitle>
+				<CardDescription className='text-pretty text-muted-foreground'>
+					{description}
+				</CardDescription>
+			</CardHeader>
+			<CardContent className='h-full'>
+				<span className='block text-center text-5xl font-bold'>
+					{monthlyPrice === 0
+						? 'Sin costo'
+						: formattedPrice
+								.format(monthlyPrice)
+								.replace(',00', '')}
+				</span>
+				<Separator className='my-6' />
+				<Link
+					href={url as unknown as UrlObject}
+					className={buttonVariants({
+						className: 'mb-6 w-full',
+						size: 'lg',
+					})}
+				>
+					Adquirir
+				</Link>
+				<ul className='space-y-1.5 text-muted-foreground'>
+					{benefits.map((benefit, i) => (
+						<li key={i}>
+							<span className='flex items-center justify-start text-pretty text-sm md:text-base'>
+								<CheckIcon
+									className='mr-4 size-4 text-green-500'
+									aria-label='Check'
+								/>
+								{benefit}
+							</span>
+						</li>
+					))}
+				</ul>
+			</CardContent>
+		</Card>
 	);
 };
