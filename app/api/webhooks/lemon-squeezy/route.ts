da7cf2bec5c29147a3a7e902c5c6bac5049f6125
pulgueta <retardix456@gmail.com>
2024-02-07 02:02:45 -0500

    Created lemon squeezy webhook to update user plan and session

--- app/api/webhooks/lemon-squeezy/route.ts
+++ app/api/webhooks/lemon-squeezy/route.ts
@@ -1,9 +1,41 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+import { env } from '~/env/server.mjs';
+import { webhookPlan } from '~/lib/products/get-plans';
+
 export const POST = async (req: NextRequest) => {
 	const body = await req.json();
 
-	console.log(body);
+	if (body.meta.event_name === 'subscription_payment_success') {
+		if (body.data.attributes.status === 'paid') {
+			console.log(body.data);
 
+			const q = await fetch(
+				`https://api.lemonsqueezy.com/v1/subscriptions/${body.data.attributes.subscription_id}`,
+				{
+					headers: {
+						Authorization: `Bearer ${env.LEMON_SQUEEZY_SECRET}`,
+					},
+				}
+			);
+
+			if (!q.ok) {
+				throw new Error('Error fetching data from Lemon Squeezy');
+			}
+
+			const res = await q.json();
+
+			console.log(
+				'Actualizando al plan:',
+				webhookPlan[res.data.attributes.product_name]
+			);
+		}
+	}
+
+	if (body.meta.event_name === 'subscription_cancelled') {
+		if (body.data.attributes.status === 'cancelled') {
+			console.log('Cancelando suscripción...');
+		}
+	}
 	return NextResponse.json(body, { status: 200 });
 };
